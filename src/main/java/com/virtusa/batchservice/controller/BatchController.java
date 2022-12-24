48059ca3ba8504e33cc13cd468f51febde66dc2e
Pavucs Manokar <smpavucsan@gmail.com>
2022-12-24 09:23:07 +0530

    controller added to the batch-service

--- src/main/java/com/virtusa/batchservice/controller/BatchController.java
+++ src/main/java/com/virtusa/batchservice/controller/BatchController.java
@@ -1,2 +1,20 @@
-package com.virtusa.batchservice.controller;public class BatchController {
+package com.virtusa.batchservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class BatchController {
+
+    @GetMapping("/batch/process/start")
+    public ResponseEntity<Map<String, String>> batchProcessStart(){
+        Map<String, String> response = new HashMap<>();
+        response.put("STATUS", "BATCH_PROCESS_STARTED");
+        return new ResponseEntity<Map<String, String>>(response, HttpStatus.OK);
+    }
 }
